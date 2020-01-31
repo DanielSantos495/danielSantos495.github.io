7755e3a3659a9a32d140d819cefded02c93b07ac
Daniel Santos <Dsantos0495@gmail.com>
2020-01-31 11:24:13 -0500

    se termin js, se hace carousel de article para portafolio, falta arreglar un bug.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,5 @@
+//Empieza el menu
+
 const menu = document.getElementById('menu__nav');
 const burguerMenu = document.getElementById("burguer-menu");
 
@@ -18,21 +20,48 @@ function showHideMenu()
     
 }
 
+//Finaliza el menu
+
+//Empieza el Carousel
+
 const imgCarouUno = document.getElementById('imgCarouUno');
-// const imgPort = document.getElementById('imgPort');
+const imgCarouDos = document.getElementById('imgCarouDos');
+const imgCarouTres = document.getElementById('imgCarouTres');
+const imgCarouCuatro = document.getElementById('imgCarouCuatro');
 
-imgCarouUno.addEventListener('mouseover',  showImgPortfolio);
-imgCarouUno.addEventListener('mouseout', hideImgPortfolio);
 
-function showImgPortfolio()
+imgCarouUno.addEventListener('mouseover',  showImgPortfolioUno);
+imgCarouDos.addEventListener('mouseover',  showImgPortfolioDos);
+imgCarouTres.addEventListener('mouseover',  showImgPortfolioTres);
+imgCarouCuatro.addEventListener('mouseover',  showImgPortfolioCuatro);
+
+
+function showImgPortfolioUno()
 {
     document.getElementById('imgPort').src = 'https://i.imgur.com/WY3yEhY.png';
 }
 
-function hideImgPortfolio()
+function showImgPortfolioDos()
 {
-    
+    document.getElementById('imgPort').src = 'https://i.imgur.com/cIrrPKp.png';
 }
 
+function showImgPortfolioTres()
+{
+    document.getElementById('imgPort').src = 'https://i.imgur.com/w9Cfw9E.png';
+}
+
+function showImgPortfolioCuatro()
+{
+    document.getElementById('imgPort').src = 'https://i.imgur.com/LvrAfYj.png';
+}
+
+const carouselContainer = document.getElementById('carouselContainer');
+carouselContainer.addEventListener('mouseout', showPhotoPrincipal);
+
+function showPhotoPrincipal()
+{
+    document.getElementById('imgPort').src = 'https://i.imgur.com/K945W7z.jpg';
+}
 
 
